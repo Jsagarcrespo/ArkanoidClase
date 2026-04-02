d54148886b4cb3cfa84006e138c83f560801ea9a
JonSagarCrespo <jonsagar.crespo@ikasle.egibide.org>
2026-04-02 05:56:40 +0200

    barra de vuelta

--- ArkanoidBreak/Assets/Scripts/Barra.cs
+++ ArkanoidBreak/Assets/Scripts/Barra.cs
@@ -7,14 +7,18 @@ public class Barra : MonoBehaviour
 
     // VELOCIDAD A LA QUE QUEREMOS LA BARRA
     public float velocidadBarra = 65f;
-    private Rigidbody2D RB; 
-    
+    private Rigidbody2D RB;
+
+    private float anchoBarra;
+
     void Start()
     {
         RB = GetComponent<Rigidbody2D>();
         RB.bodyType = RigidbodyType2D.Kinematic;
         RB.gravityScale = 0f;
         RB.freezeRotation = true;
+
+
     }
 
     // Update is called once per frame
@@ -29,6 +33,19 @@ public class Barra : MonoBehaviour
         else if (keyboard.dKey.isPressed || keyboard.rightArrowKey.isPressed)
             movimientoX = 1f;
 
-        RB.MovePosition(RB.position + new Vector2(movimientoX * velocidadBarra * Time.deltaTime, 0));
+        // para moverlo horizontalmente
+        Vector2 nuevaPos = RB.position + new Vector2(movimientoX * velocidadBarra * Time.deltaTime, 0f);
+
+        // Para que no salga de nuestra camara
+        float distHorizontal = Camera.main.orthographicSize * Screen.width / Screen.height;
+
+        // Volvemos a meter la barra si se nos sale de la camara
+        if (nuevaPos.x - anchoBarra > distHorizontal)
+            nuevaPos.x = -distHorizontal + anchoBarra;
+        else if ( nuevaPos.x + anchoBarra < - distHorizontal) 
+            nuevaPos.x = distHorizontal - anchoBarra;
+
+
+            RB.MovePosition(nuevaPos);
     }
 }
